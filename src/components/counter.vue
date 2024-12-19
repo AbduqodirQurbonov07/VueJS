cb3dd77045a0d1cef19ee20d06e419bd7aa3118f
Abduqodir <qurbonovabduqodir07@gmail.com>
2024-12-19 10:52:56 +0500

    vuega oid malumotlar

---
+++ src/components/counter.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <p>{{ sanoq }}</p>
+    <button @click="add">Oshirish</button>
+    <button id="minus" @click="minus">kamaytirish</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+let sanoq = ref(0);
+function add() {
+  sanoq.value++;
+}
+function minus() {
+  sanoq.value--;
+}
+</script>
+
+<style scoped>
+button {
+  color: white;
+  background-color: darkgreen;
+  padding: 10px 33px;
+  border-radius: 8px;
+  border: 1px solid transparent;
+}
+#minus {
+  color: white;
+  background-color: darkred;
+  padding: 10px 33px;
+  border-radius: 8px;
+  border: 1px solid transparent;
+}
+</style>

234a5749bbfdc225faabac405741c18f8713ff58
Abduqodir <qurbonovabduqodir07@gmail.com>
2025-01-06 13:09:25 +0500

    Vuedagi birinchi loyiha bajarildi

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script setup>
 // import Counter from "./components/counter.vue";
-import Light from "../src/components/light.vue";
-// import Example from "./components/example.vue";
+// import Light from "../src/components/light.vue";
+import Example from "./components/example.vue";
 </script>
 <template>
   <div>
     <!-- <Counter /> -->
-    <Light />
-    <!-- <Example /> -->
+    <!-- <Light /> -->
+    <Example />
   </div>
 </template>
 <style></style>
--- src/components/example.vue
+++ src/components/example.vue
@@ -1,42 +1,106 @@
 <template>
   <div
-    class="bg-black w-screen h-screen flex justify-center items-center flex-col gap-10"
+    class="h-screen w-screen flex flex-col items-center justify-center gap-5"
   >
-    <div class="flex items-center justify-center">
-      <img
-        v-if="istrueon"
-        class="w-150 h-50"
-        src="../images/light-on.jpg"
-        alt=""
+    <div
+      :class="[isError ? 'flex' : 'hidden']"
+      class="flex flex-col items-start w-[395px] p-2 border-red-500 border rounded-xl gap-2"
+    >
+      <p class="text-red-500 font-semibold text-lg">Xatolik</p>
+      <p class="text-red-600">Login yoki parol xato</p>
+    </div>
+
+    <div
+      :class="[isLogged && !isError ? 'flex' : 'hidden']"
+      class="flex flex-col items-start w-[395px] p-2 border-green-500 border rounded-xl gap-2"
+    >
+      <p class="text-green-600">Ro'yxatdan o'tdingiz</p>
+    </div>
+
+    <form action="" class="flex flex-col gap-3 border p-10 rounded-lg">
+      <p class="flex flex-col items-center justify-center text-black text-2xl">
+        Xush kelibsiz!
+      </p>
+      <p class="flex flex-col text-slate-500 items-center">
+        iltimos hisob malumotlarini kiriting
+      </p>
+      <p>Login</p>
+      <input
+        v-model="login"
+        class="px-4 py-1.5 border border-slate-200 rounded-lg w-80"
+        type="text"
+        placeholder="Enter you login..."
       />
-      <img v-else="" class="w-100 h-80" src="../images/light-off.jpg" alt="" />
-    </div>
-    <div class="flex gap-6">
+      <p>Parol</p>
+      <input
+        v-model="parol"
+        class="px-4 py-1.5 border border-slate-200 rounded-lg w-80"
+        type="password"
+        placeholder="Enter you password..."
+      />
+      <ul class="flex gap-2">
+        <input class="flex" type="checkbox" />
+        <label for="">eslab qolish</label>
+      </ul>
       <button
-        @click="isOn"
-        class="bg-slate-50 px-4 text-green-700 py-2 rounded-lg"
+        v-if="!isLoading"
+        @click="submitForm"
+        class="bg-indigo-600 rounded-lg text-slate-100 px-[130px] text-lg py-1"
       >
-        On
+        kirish
       </button>
       <button
-        @click="isOff"
-        class="bg-slate-50 py-2 text-red-700 px-4 rounded-lg"
+        v-else
+        @click="submitForm"
+        class="bg-indigo-600 rounded-lg text-slate-100 px-[130px] text-lg py-1"
       >
-        Off
+        Loading...
       </button>
-    </div>
+    </form>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, reactive } from "vue";
+let isLogged = ref(false);
+let login = ref("");
+let isLoading = ref(false);
+let isError = ref(false);
+let parol = ref("");
+let users = reactive([
+  { parol: "1234567", login: "Abubakr" },
+  { parol: "12345", login: "Abduqodir" },
+  { parol: "123", login: "MuhamadRajab" },
+]);
+function submitForm() {
+  isLoading.value = true;
+  setTimeout(() => {
+    const user = users.find(
+      (user) => user.login === login.value && user.parol === parol.value
+    );
+    if (user) {
+      isLogged.value = true;
+      isError.value = false;
+    } else {
+      isLogged.value = false;
+      isError.value = true;
+    }
 
-let istrueon = ref(felse);
-
-function isOn() {
-  istrueon.value = true;
-}
-function isOff() {
-  istrueon.value = false;
+    isLoading.value = false;
+    login.value = "";
+    parol.value = "";
+    setTimeout(() => {
+      isLogged.value = false;
+      isError.value = false;
+    }, 5000);
+  }, 3000);
 }
 </script>
+
+<style scoped>
+@import url(https://fonts.googleapis.com/css2?family=Kanit:wght@100;200;300;400;500;600;800;900&display=swap);
+
+body {
+  font-family: "Kanit", sans-serif;
+}
+</style>
